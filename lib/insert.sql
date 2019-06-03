77dc441b3c7a77df547bad618d98d467275f74c7
Kari O'Neal <triadfirewood@gmail.com>
2019-06-03 15:45:45 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -7,7 +7,14 @@ CREATE TABLE projects(
   end_date TEXT
 
 
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES()
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES('Horse Camp', 'Fun', 100, 1)
+
+
+
+
+
+
+
   
  INSERT INTO users (name, age) VALUES()
   
