6ccb8062f24c538b34b55605d905f2dcef963b71
Kari O'Neal <triadfirewood@gmail.com>
2019-06-03 15:44:45 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,7 +1,7 @@
 
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES()
   
- INSERT INTO users (id, name, age) VALUES
+ INSERT INTO users (name, age) VALUES()
   
   
   INSERT INTO pledges (id, amount, user_id, project_id) VALUES
