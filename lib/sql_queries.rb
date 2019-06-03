8c00179c45cac6c857d3df4307d3a3b585a2aafc
Kari O'Neal <triadfirewood@gmail.com>
2019-06-03 18:15:50 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -44,6 +44,8 @@ end
 
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
- "SELECT projects.category, SUM(pledges.amount) FROM projects
-   "
+ "SELECT projects.category, SUM(pledges.amount) AS total FROM projects
+    INNER JOIN pledges
+      ON projects.id = pledges.project_id
+    WHERE category = 'Books';"
 end
