0febbaa41fa46522d63aa0f352052dd3fc3bee5d
Kari O'Neal <triadfirewood@gmail.com>
2019-06-03 17:35:33 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -20,9 +20,10 @@ GROUP BY users.name"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-"SELECT projects.title,  projects.funding_goal -SUM(pledges.amount)  FROM projects
+"SELECT projects.title,  projects.funding_goal -SUM(pledges.amount)  FROM projects AS over_goal
 INNER JOIN pledges ON projects.id = pledges.project_id
-GROUP BY projects.title"
+GROUP BY projects.title
+HAVING SUM(pledges.amount) > projects.funding_goal"
 
 end
 
