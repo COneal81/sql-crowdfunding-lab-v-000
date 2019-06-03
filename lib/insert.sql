fecf548876facd1c4418ae36984b5075978507d1
Kari O'Neal <triadfirewood@gmail.com>
2019-06-03 15:56:25 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,15 +1,14 @@
 
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES
-('Help me buy a horse', 'fun', 1500.00, '2014-06-30', '2014-07-30'),
-('My book on Ruby', 'books', 40.00, '2014-01-01', '2015-01-01'),
-('The next Horse', 'books', 10.00, '2014-05-30', '2016-05-30'),
-('Animal shelter needs horse food', 'charity', 1400.00, '2014-03-14', '2016-06-30'),
-('Hudson needs a bone', 'charity', 6.00, '2014-03-20', '2016-09-20'),
-('The next Car', 'research', 2000000.00, '2015-12-30', '2017-12-30'),
-('Dog needs tail operation', 'charity', 3000.00, '2014-10-02', '2014-10-30'),
-('Homeless person needs a bed!', 'charity', 300.00, '2015-06-30', '2015-06-31'),
-('I want to teach Ruby to Children', 'charity', 4000.00, '2014-06-30', '2014-09-30'),
-('Help save dogs living in floodwaters', 'charity', 15000.00, '2014-03-20', '2015-06-30');
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES('Help me buy a horse', 'fun', 1500.00, '2014-06-30', '2014-07-30');
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES('My book on Ruby', 'books', 40.00, '2014-01-01', '2015-01-01');
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES('The next Horse', 'books', 10.00, '2014-05-30', '2016-05-30');
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES('Animal shelter needs horse food', 'charity', 1400.00, '2014-03-14', '2016-06-30');
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES('Hudson needs a bone', 'charity', 6.00, '2014-03-20', '2016-09-20');
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES('The next Car', 'research', 2000000.00, '2015-12-30', '2017-12-30');
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES('Dog needs tail operation', 'charity', 3000.00, '2014-10-02', '2014-10-30');
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES('Homeless person needs a bed!', 'charity', 300.00, '2015-06-30', '2015-06-31');
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES('I want to teach Ruby to Children', 'charity', 4000.00, '2014-06-30', '2014-09-30');
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES('Help save dogs living in floodwaters', 'charity', 15000.00, '2014-03-20', '2015-06-30');
 
 
 
