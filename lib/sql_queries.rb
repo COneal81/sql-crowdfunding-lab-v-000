2a6ccc5182a7dd9bf2455de1d469b5bdfe02ff54
Kari O'Neal <triadfirewood@gmail.com>
2019-06-03 16:45:31 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -7,8 +7,8 @@
 # Make sure each ruby method returns a string containing a valid SQL statement.
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
-"SELECT projects.title,  FROM projects
-JOIN 
+"SELECT projects.title, pledge.amounts FROM projects
+INNER JOIN pledge
 ON 
 ORDER BY "
 
