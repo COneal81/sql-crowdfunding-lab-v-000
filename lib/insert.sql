35f9ca04304c67c2362110d729464e6c5efe5645
Kari O'Neal <triadfirewood@gmail.com>
2019-06-03 15:49:45 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -8,16 +8,16 @@ CREATE TABLE projects(
 
 
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES
-(1,'Help me buy a guitar', 'music', 500.00, '2013-06-30', '2013-07-30'),
-(2, 'My book on SQL', 'books', 20.00, '2013-01-01', '2014-01-01'),
-(3, 'The next Harry Potter', 'books', 1000.00, '2013-05-30', '2015-05-30'),
-(4, 'Animal shelter needs dog food', 'charity', 400.00, '2013-03-14', '2013-06-30'),
-(5, 'Voldement needs a body', 'charity', 6000.00, '2013-03-20', '2013-09-20'),
-(6, 'The next Inna-Gadda-Davida', 'music', 200.00, '2014-12-30', '2015-12-30'),
-(7, 'Iguana needs tail operation', 'charity', 2000.00, '2013-10-02', '2013-10-30'),
-(8, 'I have bed bugs!', 'charity', 800.00, '2014-06-30', '2014-06-31'),
-(9, 'I want to teach English in China', 'charity', 3000.00, '2013-06-30', '2013-09-30'),
-(10, 'Help save birds of paradise', 'charity', 5000.00, '2012-03-20', '2013-06-30');
+(1,'Help me buy a horse', 'fun', 1500.00, '2014-06-30', '2014-07-30'),
+(2, 'My book on Ruby', 'books', 40.00, '2014-01-01', '2015-01-01'),
+(3, 'The next Horse', 'books', 10.00, '2014-05-30', '2016-05-30'),
+(4, 'Animal shelter needs horse food', 'charity', 1400.00, '2014-03-14', '2016-06-30'),
+(5, 'Hudson needs a body', 'charity', 6000.00, '2014-03-20', '2016-09-20'),
+(6, 'The next Inna-Gadda-Davida', 'music', 200.00, '2015-12-30', '2017-12-30'),
+(7, 'Iguana needs tail operation', 'charity', 2000.00, '2014-10-02', '2014-10-30'),
+(8, 'I have bed bugs!', 'charity', 800.00, '2015-06-30', '2015-06-31'),
+(9, 'I want to teach English in China', 'charity', 3000.00, '2014-06-30', '2014-09-30'),
+(10, 'Help save birds of paradise', 'charity', 5000.00, '2014-03-20', '2015-06-30');
 
 
 
