624d5b297b9e5a0649e31ac8ea4b4aaf2bb22dd4
Kari O'Neal <triadfirewood@gmail.com>
2019-06-03 16:48:02 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -8,7 +8,7 @@
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
 "SELECT projects.title, pledge.amounts FROM projects
-INNER JOIN pledges ON 
+INNER JOIN pledges ON project.id = pledges.
 ORDER BY "
 
 ORDER BY name
