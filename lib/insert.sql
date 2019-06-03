091eb86fe5b01effa45425191dde237477d94503
Kari O'Neal <triadfirewood@gmail.com>
2019-06-03 15:44:57 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,3 +1,11 @@
+CREATE TABLE projects(
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  category TEXT,
+  funding_goal INTEGER,
+  start_date TEXT,
+  end_date TEXT
+
 
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES()
   
