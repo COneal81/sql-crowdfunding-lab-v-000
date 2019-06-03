17f034d51169f005eccdf10d41fb68829115f819
Kari O'Neal <triadfirewood@gmail.com>
2019-06-03 15:37:14 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -9,11 +9,13 @@ CREATE TABLE projects(
 
 
 CREATE TABLE users(
+  id INTEGER PRIMARY KEY,
   name TEXT,
   age INTEGER
 );
 
 
 CREATE TABLE pledges(
+  id INTEGER PRIMARY KEY,
   amount INTEGER
 );
